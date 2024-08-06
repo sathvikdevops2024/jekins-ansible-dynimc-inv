efcc51a88856f4e3c73c7cfbe162d405af4dc5fa
SathvikGithub <166804176+SathvikGithub@users.noreply.github.com>
2024-08-06 18:13:01 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,13 +2,12 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-
 resource "aws_instance" "AWSServer"{
-ami = "ami-02bb7d8191b50f4bb"
+ami = "ami-025fe52e1f2dc5044"
 instance_type = "t2.micro"
 security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
+key_name = "sathvikdevops"
 tags = {
-Name =  "tomcatservers"
+Name = "tomcatservers"
 }
 }
